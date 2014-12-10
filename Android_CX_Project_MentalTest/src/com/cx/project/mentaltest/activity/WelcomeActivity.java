eb18e9a15ec5d7281df1d73058934653f7d4e86c
Chenxian <313733081@qq.com>
2014-12-10 19:34:57 +0800

    新增欢迎界面
    1.使用倒计时，完成欢迎界面到主界面的跳转。

---
+++ Android_CX_Project_MentalTest/src/com/cx/project/mentaltest/activity/WelcomeActivity.java
@@ -0,0 +1,48 @@
+package com.cx.project.mentaltest.activity;
+
+import com.cx.project.mentaltest.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+
+public class WelcomeActivity extends Activity{
+	private static final String TAG ="WelcomeActivity";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_welcome);
+		
+		/**
+		 * 第一个参数 ： 总时间
+		 * 第二个参数：隔多久执行一次 onTick 方法
+		 * 参数为5000,1000，是，onTick执行3次，onFinish执行1次。
+		 */
+		new CountDownTimer(3000,3000) {
+			
+			@Override
+			public void onTick(long millisUntilFinished) {
+			}
+			
+			@Override
+			public void onFinish() {
+				toMainActivity();
+				finish();
+			}
+
+		}.start();
+		
+		
+	}
+	
+	private void toMainActivity() {
+		
+		startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+		
+	}
+
+}
